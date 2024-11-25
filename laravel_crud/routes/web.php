ee558c0da0802c9dd16789eca32ca046da20be4d
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-11-25 23:12:30 +0530

    setup project and connect  data base

---
+++ laravel_crud/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaffControler;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/staff',[StaffControler :: class ,'index'])->name('staff.index');
+Route::get('/staff/create',[StaffControler :: class ,'create'])->name('staff.create');
